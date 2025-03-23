f376268141bd0d3ed443f65adfc7aa2ee5bb0b1c
Frank DiGirolomo <frank.digirolomo@gmail.com>
2025-03-23 15:36:01 -0400

    header tag

--- components/recursionunit-header.vue
+++ components/recursionunit-header.vue
@@ -2,6 +2,7 @@
     <div class="navbar bg-black">
         <div class="flex-1">
             <a class="btn btn-ghost text-xl">recursionunit</a>
+            <p class="text-sm ml-5">dev | data | cloud | security</p>
         </div>
         <div class="flex-none">
             <button class="btn">
