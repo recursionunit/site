ae118cbb407a17e32233213a158fc2228372aaf1
Frank DiGirolomo <frank.digirolomo@gmail.com>
2025-03-23 15:24:20 -0400

    center footer

--- components/recursionunit-footer.vue
+++ components/recursionunit-footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="bg-black">
-        <p>&copy;2025 recursionunit | all rights reserved</p>
-    </div>
+    <footer class="footer sm:footer-horizontal footer-center bg-black text-base-content p-4">
+        <aside>
+            <p>&copy;2025 recursionunit | all rights reserved</p>
+        </aside>
+    </footer>
 </template>
