0aa3c344f32bef3369dcaf7930d623f497c26f01
Frank DiGirolomo <frank.digirolomo@gmail.com>
2025-03-23 11:57:51 -0400

    initial

---
+++ components/recursionunit-header.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="m-3">
+        <h1 class="font-bold">recursionunit</h1>
+        <a href="mailto:info@recursionunit.io">info@recursionunit.io</a>
+    </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <RecursionunitHeader />
+        <div class="h-screen bg-cover bg-no-repeat" style="background-image: url('./images/computing-fields.png');">
+            <slot />
+        </div>
+    </div>
+</template>
