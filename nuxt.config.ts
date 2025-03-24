1f744d33e04e6e98d439b0965339fd122895c011
Frank DiGirolomo <frank.digirolomo@gmail.com>
2025-03-24 15:24:04 -0400

    switched to seo module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxtjs/tailwindcss', 'nuxt-gtag', '@nuxtjs/sitemap', '@nuxtjs/robots'],
+  modules: [
+    //'@nuxt/content', 
+    '@nuxt/eslint', 
+    '@nuxt/fonts', 
+    '@nuxt/icon', 
+    '@nuxt/image', 
+    '@nuxtjs/tailwindcss', 
+    'nuxt-gtag', 
+    '@nuxtjs/seo'
+  ],
   css: ["assets/app.css"],
   app: {
     head: {
