ccadfcd341fae6b6a03c1570cf6a2acb0dc45b19
Frank DiGirolomo <frank.digirolomo@gmail.com>
2025-03-24 15:12:37 -0400

    robots

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxtjs/tailwindcss', 'nuxt-gtag', '@nuxtjs/sitemap'],
+  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxtjs/tailwindcss', 'nuxt-gtag', '@nuxtjs/sitemap', '@nuxtjs/robots'],
   css: ["assets/app.css"],
   app: {
     head: {
