3df01ab0c9fddf6b9e225496074eeb1ee2bbfcf4
Frank DiGirolomo <frank.digirolomo@gmail.com>
2025-03-25 10:26:58 -0400

    footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,12 @@
 <template>
-    <div>
+    <div class="flex flex-col h-screen">
+            <RecursionunitHeader />
         <div class="h-screen bg-cover bg-no-repeat bg-black"
             style="background-image: url('./images/computing-fields.png');">
-            <RecursionunitHeader />
             <div class="m-5">
                 <slot />
             </div>
         </div>
+        <RecursionunitFooter />
     </div>
 </template>
