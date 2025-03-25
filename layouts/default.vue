44ab48fcfbc642452c3e3c3289cc55480dc107a2
Frank DiGirolomo <frank.digirolomo@gmail.com>
2025-03-25 12:29:13 -0400

    cleanup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col h-screen">
-            <RecursionunitHeader />
+        <RecursionunitHeader />
         <div class="h-screen bg-cover bg-no-repeat bg-black"
             style="background-image: url('./images/computing-fields.png');">
             <div class="m-5">
