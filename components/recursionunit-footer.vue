67ea7069d139a21eeb8300a9ac5ee8d2e978fc3f
Frank DiGirolomo <frank.digirolomo@gmail.com>
2025-03-26 11:04:38 -0400

    link

--- components/recursionunit-footer.vue
+++ components/recursionunit-footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="footer sm:footer-horizontal footer-center bg-black text-xs p-4 sticky bottom-0">
         <aside>
-            <p>&copy;2025 <a href="/about">recursionunit</a> | all rights reserved</p>
+            <p>&copy;2025 <NuxtLink to="/about">recursionunit</NuxtLink> | all rights reserved</p>
         </aside>
     </footer>
 </template>
